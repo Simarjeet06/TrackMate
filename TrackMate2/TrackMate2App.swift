57c30dde88666aa949aa1a28ba4b0f6e70ce4d78
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-07-27 14:27:00 +0530

    Initial Commit

---
+++ TrackMate2/TrackMate2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMate2App.swift
+//  TrackMate2
+//
+//  Created by Simarjeet Kaur on 27/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackMate2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
